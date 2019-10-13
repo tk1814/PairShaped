2df4b3440c62dd59909954b6e48668bf5f5182ab
izuane <52047724+izuane@users.noreply.github.com>
2019-10-13 15:36:36 +0100

    Add files via upload

--- welcome_get.php
+++ welcome_get.php
@@ -5,13 +5,27 @@ Welcome <?php echo $_GET["name"]; ?><br>
 Interests is: <?php echo $_GET["interests"];?><br>
 Skills is: <?php echo $_GET["skills"];?><br>
 
-Here are some matches
+Here are some matches<br>
 
 <?php
+$usrInterests = $_GET["interests"];
+
 $mikeArr = array("Mike", "Japanese", "English");
 $jamesArr = array("James", "English", "Japanese");
 
-echo $mikeArr[0];
+$peopleArray = array($mikeArr, $jamesArr);
+
+$matchFound = false;
+for ($x = 0; $x < count($peopleArray); $x++) {
+    if (strtolower($usrInterests) == strtolower($peopleArray[$x][2])){
+        echo "found a match with " . $peopleArray[$x][0] . " <br>";
+        $matchFound = true;
+    }
+}
+
+if (!$matchFound) {
+    echo "No matches found. Sorry.<br>";
+}
 
 ?>
 
